0b99cddfa60125203d6c953ef3f2aae537c7f39d
Mayank Thakur <thakurmayank88@gmail.com>
2019-10-08 19:46:38 +0530

    Init models

--- npo_backend/api/models.py
+++ npo_backend/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from mongoengine import *
 # Create your models here.
+
+
+class User(Document):
+    name = StringField(required=True)
+    email = EmailField(required=True)
+    password = StringField(required=True)
--- npo_backend/npo_backend/settings.py
+++ npo_backend/npo_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'clsl5mr312m-79ftmozv1*!f1m481sgq_(*ojh9sl=r67hl_3g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}   
