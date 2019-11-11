5666b3af5323e6b713e5dfc667b5306b3bb1d57c
Mudit <muditj06@gmail.com>
2019-11-11 15:38:26 +0530

    Model methodupdate

--- npo_backend/api/models.py
+++ npo_backend/api/models.py
@@ -17,6 +17,9 @@ class UserProfile(models.Model):
     pan_no = models.TextField(blank=True, null=True)
     aadhar_no = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return super().__str__() + f': {self.user.username}'
+
 
 class Event(models.Model):
     name = models.CharField(max_length=255, blank=False)
